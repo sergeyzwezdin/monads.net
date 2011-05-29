930330809b7491ef66c6878227715ce2351ab2e2
Sergey Zwezdin <Sergey Zwezdin@OOGWAY>
2011-05-30 01:04:10 +0600

    Monads for IEnumerable

---
+++ Monads.Tests/MaybeIEnumerableTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Monads.Tests
+{
+	[TestClass]
+	public class MaybeIEnumerableTests
+	{
+		[TestMethod]
+		public void DoWithNotEmpty()
+		{
+			var source = new[] { "a1", "a2,", "a3" };
+			var result = new List<string>();
+
+			source.Do(s => result.Add(s));
+
+			Assert.AreEqual(source.Length, result.Count);
+			Assert.AreEqual(source[0], result[0]);
+			Assert.AreEqual(source[1], result[1]);
+			Assert.AreEqual(source[2], result[2]);
+		}
+
+		[TestMethod]
+		public void DoWithEmpty()
+		{
+			string[] source = null;
+			var result = new List<string>();
+
+			source.Do(s => result.Add(s));
+
+			Assert.AreEqual(0, result.Count);
+		}
+
+		[TestMethod]
+		public void WithNotEmpty()
+		{
+			var source = new[] { "a1", "a2,", "a3" };
+			var result = source.With((string s) => s + "b").ToArray();
+
+			Assert.AreEqual(source.Count(), result.Count());
+			Assert.AreEqual(source[0] + "b", result[0]);
+			Assert.AreEqual(source[1] + "b", result[1]);
+			Assert.AreEqual(source[2] + "b", result[2]);
+		}
+
+		[TestMethod]
+		public void WithEmpty()
+		{
+			string[] source = null;
+			var result = source.With(s => s + "b");
+
+			Assert.IsNull(result);
+		}
+	}
+}
--- Monads/Maybe.IEnumerable.cs
+++ Monads/Maybe.IEnumerable.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace System.Monads
 {
 	public static class MaybeIEnumerable
 	{
+		public static IEnumerable Do(this IEnumerable source, Action<object> action)
+		{
+			if (source != null)
+			{
+				foreach (var element in source)
+				{
+					action(element);
+				}
+			}
+
+			return source;
+		}
+
+		public static IEnumerable<TSource> Do<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+		{
+			if (source != null)
+			{
+				foreach (var element in source)
+				{
+					action(element);
+				}
+			}
+
+			return source;
+		}
+
+		public static IEnumerable<TResult> With<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> action)
+		{
+			if (source != null)
+			{
+				return source.Select(action);
+			}
+			else
+			{
+				return null;
+			}
+		}
 	}
 }
