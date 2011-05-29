6b8ba5e5ff3eaa48f7536416a64d297865f5270f
Sergey Zwezdin <Sergey Zwezdin@OOGWAY>
2011-05-29 17:50:31 +0600

    Initial sources

---
+++ Monads.Tests/MaybeObjectsTests.cs
@@ -0,0 +1,320 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Monads.Tests
+{
+	[TestClass]
+	public class MaybeObjectsTests
+	{
+		[TestMethod]
+		public void DoOnObjectWithValue()
+		{
+			var source = new { Property = "Some value" };
+
+			var result = String.Empty;
+			source.Property.Do(s => result = s);
+
+			Assert.AreEqual("Some value", result);
+		}
+
+		[TestMethod]
+		public void DoOnObjectWithNull()
+		{
+			var source = new { Property = (string)null };
+
+			var result = String.Empty;
+			source.Property.Do(s => result = s);
+
+			Assert.AreEqual(String.Empty, result);
+		}
+
+		[TestMethod]
+		public void WithOnObjectWithValue()
+		{
+			var source = new { Property1 = new { Property2 = new { Property3 = "Some value" } } };
+
+			var result = source.With(s => s.Property1).With(s => s.Property2).With(s => s.Property3);
+
+			Assert.AreEqual("Some value", result);
+		}
+
+		[TestMethod]
+		public void WithOnObjectWithNull()
+		{
+			var source = new { Property1 = new { Property2 = (object)null } };
+
+			var result = source.With(s => s.Property1).With(s => s.Property2);
+
+			Assert.AreEqual(null, result);
+		}
+
+		[TestMethod]
+		public void ReturnOnObjectWithValue()
+		{
+			var source = new { Property = "Some value" };
+
+			var result = source.Return(s => s.Property, "Nothing");
+
+			Assert.AreEqual("Some value", result);
+		}
+
+		[TestMethod]
+		public void ReturnOnObjectWithNull()
+		{
+			var source = new { Property = "Some value" };
+			source = null;
+
+			var result = source.Return(s => s.Property, "Nothing");
+
+			Assert.AreEqual("Nothing", result);
+		}
+
+		[TestMethod]
+		public void IfOnObjectWithValue()
+		{
+			var source = new { Property = 5 };
+
+			Assert.AreEqual(source, source.If(s => s.Property > 3));
+			Assert.AreEqual(null, source.If(s => s.Property > 6));
+		}
+
+		[TestMethod]
+		public void IfOnObjectWithNull()
+		{
+			var source = new { Property = 5 };
+			source = null;
+
+			Assert.IsNull(source.If(s => s.Property > 3));
+			Assert.IsNull(source.If(s => s.Property > 6));
+		}
+
+		[TestMethod]
+		public void IfNotOnObjectWithValue()
+		{
+			var source = new { Property = 5 };
+
+			Assert.AreEqual(null, source.IfNot(s => s.Property > 3));
+			Assert.AreEqual(source, source.IfNot(s => s.Property > 6));
+		}
+
+		[TestMethod]
+		public void IfNotOnObjectWithNull()
+		{
+			var source = new { Property = 5 };
+			source = null;
+
+			Assert.IsNull(source.IfNot(s => s.Property > 3));
+			Assert.IsNull(source.IfNot(s => s.Property > 6));
+		}
+
+		[TestMethod]
+		public void RecoverOnObjectWithValue()
+		{
+			string source = "Some value";
+			source = null;
+
+			var result = source.Recover(() => "Default");
+
+			Assert.AreEqual("Default", result);
+		}
+
+		[TestMethod]
+		public void OfTypeOnString()
+		{
+			object source = "Some value";
+
+			string result = source.OfType<string>();
+
+			Assert.AreEqual("Some value", result);
+		}
+
+		[TestMethod]
+		public void OfTypeOnInt()
+		{
+			object source = 5;
+
+			int result = source.OfType<int>();
+
+			Assert.AreEqual(5, result);
+		}
+
+		[TestMethod]
+		public void OfTypeOnIntVsString()
+		{
+			object source = 5;
+
+			string result = source.OfType<string>();
+
+			Assert.AreEqual(null, result);
+		}
+
+		[TestMethod]
+		public void TryDoOnObjectWithValueNoException()
+		{
+			var source = new { Property = "Some value" };
+
+			var r = String.Empty;
+			var result = source.TryDo(s => r = s.Property);
+
+			Assert.AreEqual("Some value", r);
+			Assert.AreEqual(source, result.Item1);
+			Assert.AreEqual(null, result.Item2);
+		}
+
+		[TestMethod]
+		public void TryDoOnObjectWithNullNoException()
+		{
+			var source = new { Property = "Some value" };
+			source = null;
+
+			var r = String.Empty;
+			var result = source.TryDo(s => r = s.Property);
+
+			Assert.AreEqual(String.Empty, r);
+			Assert.AreEqual(null, result.Item1);
+			Assert.AreEqual(null, result.Item2);
+		}
+
+		[TestMethod]
+		public void TryDoOnObjectWithException()
+		{
+			var source = new { Property = (string)null };
+
+			var r = String.Empty;
+			var result = source.TryDo(s => r = s.Property.ToString());
+
+			Assert.AreEqual(String.Empty, r);
+			Assert.AreEqual(source, result.Item1);
+			Assert.IsInstanceOfType(result.Item2, typeof(NullReferenceException));
+		}
+
+		[TestMethod]
+		public void TryDoOnObjectWithExceptionImplicitLambda()
+		{
+			var source = new { Property = (string)null };
+
+			var result = source.TryDo(s => s.Property.ToString(), ex => ex is NullReferenceException);
+
+			Assert.AreEqual(source, result.Item1);
+			Assert.IsInstanceOfType(result.Item2, typeof(NullReferenceException));
+		}
+
+		[TestMethod]
+		public void TryDoOnObjectWithExceptionImplicitArray()
+		{
+			var source = new { Property = (string)null };
+
+			var result = source.TryDo(s => s.Property.ToString(), typeof(NullReferenceException), typeof(ArgumentException));
+
+			Assert.AreEqual(source, result.Item1);
+			Assert.IsInstanceOfType(result.Item2, typeof(NullReferenceException));
+		}
+
+		[TestMethod]
+		public void TryDoOnObjectWithExceptionImplicitArrayFails()
+		{
+			try
+			{
+				var source = new { Property = (string)null };
+
+				var result = source.TryDo(s => s.Property.ToString(), typeof(OutOfMemoryException), typeof(ArgumentException));
+
+				Assert.Fail("Exception must be thrown.");
+			}
+			catch (NullReferenceException)
+			{
+			}
+		}
+
+
+		[TestMethod]
+		public void TryWithOnObjectWithException()
+		{
+			var source = new { Property1 = (string)null };
+
+			var result = source.TryWith(s => s.Property1.ToString());
+
+			Assert.AreEqual(null, result.Item1);
+			Assert.IsInstanceOfType(result.Item2, typeof(NullReferenceException));
+		}
+
+		[TestMethod]
+		public void TryWithOnObjectWithExceptionLambda()
+		{
+			var source = new { Property1 = (string)null };
+
+			var result = source.TryWith(s => s.Property1.ToString(), ex => ex is NullReferenceException);
+
+			Assert.AreEqual(null, result.Item1);
+			Assert.IsInstanceOfType(result.Item2, typeof(NullReferenceException));
+		}
+
+		[TestMethod]
+		public void TryWithOnObjectWithExceptionImplicitArray()
+		{
+			var source = new { Property1 = (string)null };
+
+			var result = source.TryWith(s => s.Property1.ToString(), typeof(NullReferenceException));
+
+			Assert.AreEqual(null, result.Item1);
+			Assert.IsInstanceOfType(result.Item2, typeof(NullReferenceException));
+		}
+
+		[TestMethod]
+		public void TryWithOnObjectWithExceptionImplicitArrayFails()
+		{
+			try
+			{
+				var source = new { Property1 = (string)null };
+
+				var result = source.TryWith(s => s.Property1.ToString(), typeof(OutOfMemoryException));
+
+				Assert.Fail("Exception must be thrown.");
+			}
+			catch (NullReferenceException)
+			{
+			}
+		}
+
+		[TestMethod]
+		public void CatchIgnore()
+		{
+			var source = new Tuple<string, Exception>("Some value", new ArgumentException());
+
+			var result = source.Catch();
+
+			Assert.AreEqual("Some value", result);
+		}
+
+		[TestMethod]
+		public void CatchLog()
+		{
+			var source = new Tuple<string, Exception>("Some value", new ArgumentException());
+
+			Exception resultException = null;
+			var result = source.Catch(ex => resultException = ex);
+
+			Assert.AreEqual("Some value", result);
+			Assert.IsInstanceOfType(resultException, typeof(ArgumentException));
+		}
+
+		[TestMethod]
+		public void AnyTrue()
+		{
+			var source = "Some value";
+
+			var result = source.Any();
+
+			Assert.AreEqual(true, result);
+		}
+
+		[TestMethod]
+		public void AnyFalse()
+		{
+			string source = null;
+
+			var result = source.Any();
+
+			Assert.AreEqual(false, result);
+		}
+	}
+}
---
+++ Monads/Maybe.Objects.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace System.Monads
+{
+	public static class MaybeObjects
+	{
+		public static TSource Do<TSource>(this TSource source, Action<TSource> action)
+			where TSource : class
+		{
+			if (source != default(TSource))
+			{
+				action(source);
+			}
+
+			return source;
+		}
+
+		public static TResult With<TSource, TResult>(this TSource source, Func<TSource, TResult> action)
+			where TSource : class
+		{
+			if (source != default(TSource))
+			{
+				return action(source);
+			}
+			else
+			{
+				return default(TResult);
+			}
+		}
+
+		public static TResult Return<TSource, TResult>(this TSource source, Func<TSource, TResult> action, TResult defaultValue)
+			where TSource : class
+		{
+			if (source != default(TSource))
+			{
+				return action(source);
+			}
+			else
+			{
+				return defaultValue;
+			}
+		}
+
+		public static TSource If<TSource>(this TSource source, Func<TSource, bool> condition)
+			where TSource : class
+		{
+			if ((source != default(TSource)) && (condition(source) == true))
+			{
+				return source;
+			}
+			else
+			{
+				return default(TSource);
+			}
+		}
+
+		public static TSource IfNot<TSource>(this TSource source, Func<TSource, bool> condition)
+			where TSource : class
+		{
+			if ((source != default(TSource)) && (condition(source) == false))
+			{
+				return source;
+			}
+			else
+			{
+				return default(TSource);
+			}
+		}
+
+		public static TInput Recover<TInput>(this TInput source, Func<TInput> action)
+			where TInput : class
+		{
+			return source ?? action();
+		}
+
+		public static TResult OfType<TResult>(this object source)
+		{
+			if (source is TResult)
+			{
+				return (TResult)source;
+			}
+			else
+			{
+				return default(TResult);
+			}
+		}
+
+		public static Tuple<TSource, Exception> TryDo<TSource>(this TSource source, Action<TSource> action)
+			where TSource : class
+		{
+			if (source != default(TSource))
+			{
+				try
+				{
+					action(source);
+				}
+				catch (Exception ex)
+				{
+					return new Tuple<TSource, Exception>(source, ex);
+				}
+			}
+
+			return new Tuple<TSource, Exception>(source, null);
+		}
+
+		public static Tuple<TSource, Exception> TryDo<TSource>(this TSource source, Action<TSource> action, Func<Exception, bool> exceptionChecker)
+			where TSource : class
+		{
+			if (source != default(TSource))
+			{
+				try
+				{
+					action(source);
+				}
+				catch (Exception ex)
+				{
+					if (exceptionChecker(ex) == true)
+					{
+						return new Tuple<TSource, Exception>(source, ex);
+					}
+					else
+					{
+						throw ex;
+					}
+				}
+			}
+
+			return new Tuple<TSource, Exception>(source, null);
+		}
+
+		public static Tuple<TSource, Exception> TryDo<TSource>(this TSource source, Action<TSource> action, params Type[] expectedEception)
+			where TSource : class
+		{
+			if (source != default(TSource))
+			{
+				try
+				{
+					action(source);
+				}
+				catch (Exception ex)
+				{
+					if (expectedEception.Any(exp => exp.IsInstanceOfType(ex)) == true)
+					{
+						return new Tuple<TSource, Exception>(source, ex);
+					}
+					else
+					{
+						throw ex;
+					}
+				}
+			}
+
+			return new Tuple<TSource, Exception>(source, null);
+		}
+
+
+		public static Tuple<TResult, Exception> TryWith<TSource, TResult>(this TSource source, Func<TSource, TResult> action)
+			where TSource : class
+		{
+			if (source != default(TSource))
+			{
+				TResult result = default(TResult);
+				try
+				{
+					result = action(source);
+					return new Tuple<TResult, Exception>(result, null);
+				}
+				catch (Exception ex)
+				{
+					return new Tuple<TResult, Exception>(result, ex);
+				}
+			}
+
+			return new Tuple<TResult, Exception>(default(TResult), null);
+		}
+
+
+		public static Tuple<TResult, Exception> TryWith<TSource, TResult>(this TSource source, Func<TSource, TResult> action, Func<Exception, bool> exceptionChecker)
+			where TSource : class
+		{
+			if (source != default(TSource))
+			{
+				TResult result = default(TResult);
+				try
+				{
+					result = action(source);
+					return new Tuple<TResult, Exception>(result, null);
+				}
+				catch (Exception ex)
+				{
+					if (exceptionChecker(ex) == true)
+					{
+						return new Tuple<TResult, Exception>(result, ex);
+					}
+					else
+					{
+						throw ex;
+					}
+				}
+			}
+
+			return new Tuple<TResult, Exception>(default(TResult), null);
+		}
+
+		public static Tuple<TResult, Exception> TryWith<TSource, TResult>(this TSource source, Func<TSource, TResult> action, params Type[] expectedEception)
+			where TSource : class
+		{
+			if (source != default(TSource))
+			{
+				TResult result = default(TResult);
+				try
+				{
+					result = action(source);
+					return new Tuple<TResult, Exception>(result, null);
+				}
+				catch (Exception ex)
+				{
+					if (expectedEception.Any(exp => exp.IsInstanceOfType(ex)) == true)
+					{
+						return new Tuple<TResult, Exception>(result, ex);
+					}
+					else
+					{
+						throw ex;
+					}
+				}
+			}
+
+			return new Tuple<TResult, Exception>(default(TResult), null);
+		}
+
+		public static TSource Catch<TSource>(this Tuple<TSource, Exception> source)
+		{
+			return source.Item1;
+		}
+
+		public static TSource Catch<TSource>(this Tuple<TSource, Exception> source, Action<Exception> handler)
+		{
+			if (source.Item2 != null)
+			{
+				handler(source.Item2);
+			}
+
+			return source.Item1;
+		}
+
+		public static bool Any<TSource>(this TSource source)
+			where TSource : class
+		{
+			return source != default(TSource);
+		}
+	}
+}
