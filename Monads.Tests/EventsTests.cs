4b20fcac64a433842efa927d18bd813a298af284
Sergey Zwezdin <Sergey Zwezdin@OOGWAY>
2011-05-31 02:15:58 +0600

    Monads for event invokation

---
+++ Monads.Tests/EventsTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Monads.Tests
+{
+	[TestClass]
+	public class EventsTests
+	{
+		internal class EventMock
+		{
+			public void InvokeEvent()
+			{
+				TestEvent.Execute(this, EventArgs.Empty);
+			}
+
+			public event EventHandler TestEvent;
+		}
+
+		internal class EventMock<TArgs>
+			where TArgs : EventArgs
+		{
+			public void InvokeEvent(TArgs arg)
+			{
+				TestEvent.Execute(this, arg);
+			}
+
+			public event EventHandler<TArgs> TestEvent;
+		}
+
+		public class EventArgsMock : EventArgs
+		{
+			public string Message { get; private set; }
+
+			public EventArgsMock(string message)
+			{
+				Message = message;
+			}
+		}
+
+		[TestMethod]
+		public void ExecuteNotGenericWithNull()
+		{
+			var eventMock = new EventMock();
+			eventMock.InvokeEvent();
+		}
+
+		[TestMethod]
+		public void ExecuteNotGenericWithNotNull()
+		{
+			bool executed = false;
+
+			var eventMock = new EventMock();
+			eventMock.TestEvent += (s, e) => { executed = true; };
+
+			eventMock.InvokeEvent();
+
+			Assert.IsTrue(executed);
+		}
+
+		[TestMethod]
+		public void ExecuteGenericWithNull()
+		{
+			var eventMock = new EventMock<EventArgsMock>();
+			eventMock.InvokeEvent(new EventArgsMock("Test"));
+		}
+
+		[TestMethod]
+		public void ExecuteGenericWithNotNull()
+		{
+			bool executed = false;
+
+			var eventMock = new EventMock<EventArgsMock>();
+			eventMock.TestEvent += (s, e) => { executed = e.Message == "Test"; };
+
+			eventMock.InvokeEvent(new EventArgsMock("Test"));
+
+			Assert.IsTrue(executed);
+		}
+	}
+}
---
+++ Monads/Events.cs
@@ -0,0 +1,26 @@
+﻿namespace System.Monads
+{
+	public static class Events
+	{
+		public static EventHandler Execute(this EventHandler source, object sender, EventArgs args)
+		{
+			if (source != null)
+			{
+				source.Invoke(sender, args);
+			}
+
+			return source;
+		}
+
+		public static EventHandler<TArgs> Execute<TArgs>(this EventHandler<TArgs> source, object sender, TArgs args)
+			where TArgs : EventArgs
+		{
+			if (source != null)
+			{
+				source.Invoke(sender, args);
+			}
+
+			return source;
+		}
+	}
+}
