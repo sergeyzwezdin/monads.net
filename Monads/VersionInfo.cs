3ba6899e3467b84e32de973a1f7dacc696368a97
Sergey Zwezdin <Sergey Zwezdin@OOGWAY.is74.ru>
2011-10-06 09:10:38 +0600

    Common versioning and signing

---
+++ Monads/VersionInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Monads for .NET")]
+[assembly: AssemblyDescription("Monads for .NET library. http://monads.codeplex.com/")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Monads for .NET")]
+[assembly: AssemblyCopyright("Copyright © Sergey Zwezdin 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
