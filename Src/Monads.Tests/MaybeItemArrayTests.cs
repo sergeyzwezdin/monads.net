2fdbac160b0e37256086e054cb69457ae6dfefdb
unknown <dkondratiuk@EMMSANBOOK18.intranet.emmsa.com.ar>
2013-05-20 17:55:59 -0300

    "At" monad implemented for IList. See MaybeItemArrayTests.cs

---
+++ Src/Monads.Tests/MaybeItemArrayTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Monads.Tests
+{
+  [TestClass]
+  public class MaybeItemArrayTests
+  {
+    [TestMethod]
+    public void DoOnArraysWithValue()
+    {
+      var source = new string[] { "One", "Two", "Three" };
+
+      var result = source.At<string>(0);
+      Assert.AreEqual("One", result);
+    }
+
+    [TestMethod]
+    public void DoOnArraysOutOfIndex()
+    {
+      var source = new string[] { "One", "Two", "Three" };
+
+      var result = source.At<string>(5);
+
+      Assert.AreEqual(null, result);
+    }
+
+    [TestMethod]
+    public void DoOnEmptyArray()
+    {
+      var source = new string[] { };
+
+      var result = source.At<string>(0);
+
+      Assert.AreEqual(null, result);
+    }
+
+
+    [TestMethod]
+    public void IsNullTrue()
+    {
+      var source = new string[] { "One", "Two", "Three" };
+
+      var result = source.At<string>(3).IsNull();
+
+      Assert.AreEqual(true, result);
+    }
+  }
+}
